8d99afdbcc7c5637550a792ff1b40c83b5ae1c46
Terri Squire <terri.m.squire@gmail.com>
2019-10-25 09:12:51 -0500

    Update range chapter

--- 09_range/main.go
+++ 09_range/main.go
@@ -11,8 +11,29 @@ func main() {
 		fmt.Printf("%d - ID: %d\n", i, id)
 	}
 
-	// Not using index
+	// Not using index - instead of "for i, use for _,"
 	for _, id := range ids {
 		fmt.Printf("ID: %d\n", id)
+	}
+
+	// Add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum:", sum)
+
+	// Range with map
+	emails := map[string]string{"Cheryl": "cheryl@she_shed.com", "Judy": "judy@shesheersheshed.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v) // %s is a placeholder for k and v
+	}
+
+	// Get just the key
+	for k := range emails {
+		fmt.Printf("%s\n", k)
+		// or
+		fmt.Println("Name: " + k)
 	}
 }
